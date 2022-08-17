41c2a8a10b913425e636c1b514be5cdf6ebb26d4
EmmanuelNC <5720100800@utrng.edu.mx>
2022-08-17 13:33:04 -0500

    Revert "Elimnando [key] en clase Cinema.cs"
    This reverts commit 610ad9a34c0edfd4335e66306f4cf354eb4efeaf.

--- PeliculaAPI/Models/Cinema.cs
+++ PeliculaAPI/Models/Cinema.cs
@@ -5,7 +5,7 @@ namespace PeliculaAPI.Models
 {
     public class Cinema
     {
-        
+        [Key]
         public int Id { get; set; }
         public string? Nombre { get; set; }
         public string? RutaImagen { get; set; }
