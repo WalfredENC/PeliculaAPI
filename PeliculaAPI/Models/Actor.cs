a4610fa8288a59c46bf06f14fd686ed096580ecd
EmmanuelNC <5720100800@utrng.edu.mx>
2022-08-17 18:42:25 -0500

    Eliminando código basura.

--- PeliculaAPI/Models/Actor.cs
+++ PeliculaAPI/Models/Actor.cs
@@ -8,17 +8,6 @@ namespace PeliculaAPI.Models
         [Key]
         public int Id { get; set; }
         public string? Nombre { get; set; }
-        public string? RutaImagen { get; set; }
-        public string? Descripcion { get; set; }
-        public string? Duracion { get; set; }
-        public enum TipoClasificacion { Siete, Trece, Dieciseis, Dieciocho }
-        public TipoClasificacion Clasificacion { get; set; }
-        public DateTime FechaCreacion { get; set; }
-        public int categoriaId { get; set; }
-        [ForeignKey("categoriaId")]
-        public Categoria Categoria { get; set; }
-
-        public int PeliculasFIlmadas {get; set;}
-
+        //Saludos amigos.
     }
 }
