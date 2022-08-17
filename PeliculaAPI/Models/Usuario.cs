3540d4499e2f2c2f8f0fc70a02d852760a947c70
EmmanuelNC <106292482+WalfredENC@users.noreply.github.com>
2022-08-17 18:16:28 -0500

    Actualizando clase Usuario.cs desde repositorio remoto.

--- PeliculaAPI/Models/Usuario.cs
+++ PeliculaAPI/Models/Usuario.cs
@@ -5,11 +5,12 @@ namespace PeliculaAPI.Models
     public class Usuario
     {
         [Key]
-        public int Id { get; set; }
+        public int Identificador {get; set;}
+        
+        public string? Nombre {get; set;}
         public string? UsuarioA { get; set; }
         public byte[]? PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-
-
+        public int Edad {get; set}
     }
 }
