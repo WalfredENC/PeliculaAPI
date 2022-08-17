87daeb8d84497e75bacfad50e3e01bd5b3450e1f
EmmanuelNC <5720100800@utrng.edu.mx>
2022-08-17 18:43:38 -0500

    13-06-22

--- PeliculaAPI/Models/Actor.cs
+++ PeliculaAPI/Models/Actor.cs
@@ -7,12 +7,6 @@ namespace PeliculaAPI.Models
     {
         [Key]
         public int Id { get; set; }
-        public string? Nombre { get; set; }
-        public string? RutaImagen { get; set; }
-        public string? Descripcion { get; set; }
-        //Emmanuel Nájera Castrejón
-        public string? Duracion { get; set; }
-
         public DateTime FechaCreacion { get; set; }
         public int categoriaId { get; set; }
         [ForeignKey("categoriaId")]
